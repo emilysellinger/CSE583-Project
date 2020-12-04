d6cacc601653aec7020bad2d2c0f926c3fdc0564
emilysellinger <71989576+emilysellinger@users.noreply.github.com>
2020-12-04 14:51:41 -0800

    Update test_travis.py

--- Phoenix/tests/test_travis.py
+++ Phoenix/tests/test_travis.py
@@ -7,7 +7,3 @@ class TestTravis(unittest.TestCase):
     def test_oneshot(self):
         number = 5
         self.AssertEqual(travis_test.add_one(number), 6)
-
-
-if __name__ == '__main__':
-    unittest.main()
