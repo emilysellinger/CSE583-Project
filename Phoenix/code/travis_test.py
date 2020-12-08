a928d0a39b281aae99a3f28f2984ccdef979b8ee
Zoe Rand <zrand@uw.edu>
2020-12-07 17:35:50 -0800

    Cleaned up repository

--- Phoenix/code/travis_test.py
+++
@@ -1,9 +0,0 @@
-""" This module contains a test to ensure our Travis CI is working properly.
-
-adds 1 to the number passed to the function
-"""
-
-
-def add_one(number):
-    answer = number + 1
-    return(answer)
